ca232d4014d09622284567fc30eb69770345e57f
Vitor Sales <vitormalisa@gmail.com>
2023-09-26 17:30:48 -0300

    Requisito 7 feito

--- src/7-search-products-by-brand.js
+++ src/7-search-products-by-brand.js
@@ -1,7 +1,25 @@
 const stockProducts = require('./data.json');
 
-const searchProductsByBrand = () => {
+const searchProductsByBrand = (brand) => {
   // Desenvolva seu código dentro dessa função...
+  // Variaveis: array
+  const produtosMarca = [];
+  // Laço de repetição para checar marca de cada produto
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    // Condicional: se marca for a procurada adiciona ao array
+    if (brand === stockProducts[index].brand) {
+      produtosMarca.push({
+        description: stockProducts[index].description,
+        formattedPrice: `R$ ${stockProducts[index].price}`
+      })
+    }
+  }
+  // O que retorna
+  return produtosMarca;
 };
+
+console.log(searchProductsByBrand('Andorinha'));
+console.log(searchProductsByBrand('Hortifruti'));
+
 
 module.exports = { searchProductsByBrand };
