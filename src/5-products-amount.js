250fb6c5fb30d29ef24c6cc746f82710d62018f0
Vitor Sales <vitormalisa@gmail.com>
2023-09-26 16:25:30 -0300

    Requisito 5 feito

--- src/5-products-amount.js
+++ src/5-products-amount.js
@@ -2,6 +2,18 @@ const stockProducts = require('./data.json');
 
 const getProductsAmount = () => {
   // Desenvolva seu código dentro dessa função...
+  // Criar variavel para receber a soma
+  let totalStock = 0;
+  // Criar laço de repetição para percorrer o estoque de cada produto e fazer a soma
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    totalStock += stockProducts[index].quantityInStock;
+  }
+  // return soma de todos os produtos
+  return totalStock;
 };
+
+getProductsAmount();
+
+
 
 module.exports = { getProductsAmount };
