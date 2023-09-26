304cdc9db78b0b1a1ac598ffd81487474552da47
Vitor Sales <vitormalisa@gmail.com>
2023-09-26 19:16:46 -0300

    Requisito 9 feito

--- src/9-products-with-allergy-or-intolerance-info.js
+++ src/9-products-with-allergy-or-intolerance-info.js
@@ -2,6 +2,31 @@ const stockProducts = require('./data.json');
 
 const getProductsWithAllergyOrIntoleranceInfo = () => {
   // Desenvolva seu código dentro dessa função...
+  // variaveis: array
+  const allergicPointedOut = [];
+  let allergicContent = 'Pode conter:';
+  // Laço de repetição procurando propriedade de alergia ou intolerancia
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    allergicPointedOut.push({
+      description: stockProducts[index].description,
+      formattedPrice: `R$ ${stockProducts[index].price}`,
+    })
+    // transformando em array as chaves de cada objeto para procurar pela allegyOrIntolerance
+    let objectKeysArray = Object.keys(stockProducts[index]);
+    // Laço de repetição para checar se existe allergyOrIntolerance
+    for (let indexKey = 0; indexKey < objectKeysArray.length; indexKey += 1) {
+      if (objectKeysArray[indexKey] === 'allergyOrIntolerance') {
+        for (let indexAllergy = 0; indexAllergy < stockProducts[index].allergyOrIntolerance.length; indexAllergy += 1) {
+           allergicContent += ` ${stockProducts[index].allergyOrIntolerance[indexAllergy]}`;
+        }
+        allergicPointedOut[index].allergyOrIntoleranceMessage = allergicContent;
+        allergicContent = 'Pode conter:';
+      }
+    }
+  }
+  return allergicPointedOut;
 };
+
+console.log(getProductsWithAllergyOrIntoleranceInfo());
 
 module.exports = { getProductsWithAllergyOrIntoleranceInfo };
