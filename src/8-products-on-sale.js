96503744b692ddb7a1d9993a33f31ea3004addb9
Vitor Sales <vitormalisa@gmail.com>
2023-09-26 17:40:46 -0300

    Requisito 8 feito

--- src/8-products-on-sale.js
+++ src/8-products-on-sale.js
@@ -2,6 +2,23 @@ const stockProducts = require('./data.json');
 
 const getProductsOnSale = () => {
   // Desenvolva seu código dentro dessa função...
+  // Variaveis: array
+  const productsOnSale = [];
+  // Laço de repetição para checagem dos produtos em promoção
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    // Condicional para se o produtor estiver em promoção seja adicionado ao novo array.
+    if (stockProducts[index].onSale === true) {
+      productsOnSale.push({
+        description: stockProducts[index].description,
+        formattedPrice: `R$ ${stockProducts[index].price}`,
+        onSale: stockProducts[index].onSale
+      })
+    }
+  }
+  // Retorno da array
+  return productsOnSale;
 };
+
+console.log(getProductsOnSale());
 
 module.exports = { getProductsOnSale };
