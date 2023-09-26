f83408b8a5e92f997ede112f23c89a2fe8838b65
Vitor Sales <vitormalisa@gmail.com>
2023-09-26 10:05:43 -0300

    Requisito 2 feito, mas falhando no teste

--- src/2-unique-products-name.js
+++ src/2-unique-products-name.js
@@ -1,7 +1,16 @@
 const stockProducts = require('./data.json');
 
-const getUniqueProductsName = () => {
+const getUniqueProductsName = (array) => {
   // Desenvolva seu código dentro dessa função...
+  // Criar um array para receber os nomes dos produtos
+  // Entrar no array pelo indice e na propriedade do objeto
+  const nomeProdutos = [];
+  for (let index = 0; index < array.length; index += 1) {
+    nomeProdutos.push(array[index].productName);
+  }
+  return nomeProdutos;
 };
+
+console.log(getUniqueProductsName(stockProducts));
 
 module.exports = { getUniqueProductsName };
