3f2098db71e26f7af5442b495e5f85e2f5180c84
Vitor Sales <vitormalisa@gmail.com>
2023-09-26 09:39:17 -0300

    Requisito feito, mas apresenta falha no teste

--- src/1-unique-products-amount.js
+++ src/1-unique-products-amount.js
@@ -1,7 +1,11 @@
 const stockProducts = require('./data.json');
 
-const getUniqueProductsAmount = () => {
+const getUniqueProductsAmount = (stockProducts) => {
   // Desenvolva seu código dentro dessa função...
+  let uniqueProducts = stockProducts.length;
+  return uniqueProducts;
 };
+
+console.log(getUniqueProductsAmount(stockProducts));
 
 module.exports = { getUniqueProductsAmount };
