35f61ab1baf9d6f3b56289c70fbfb4fe21559e33
Vitor Sales <vitormalisa@gmail.com>
2023-09-26 10:58:22 -0300

    Requisito 4 feito

--- src/4-low-stock-products.js
+++ src/4-low-stock-products.js
@@ -2,6 +2,18 @@ const stockProducts = require('./data.json');
 
 const getLowStockProducts = () => {
   // Desenvolva seu código dentro dessa função...
+  // Criar novo array
+  const estoqueBaixo = [];
+  // Criar laço de repetição para checar todos os indices de stockProducts
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    // Criar condicional para colocar no novo array produtos que estão com estoque baixo
+    if (stockProducts[index].quantityInStock > 0 && stockProducts[index].quantityInStock <= 10) {
+      estoqueBaixo.push(`${stockProducts[index].productName}: ${stockProducts[index].quantityInStock} unidades`)
+    }
+  }
+  return estoqueBaixo;
 };
+
+console.log(getLowStockProducts());
 
 module.exports = { getLowStockProducts };
