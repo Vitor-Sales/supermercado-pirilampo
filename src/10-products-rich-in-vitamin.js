d7fc3d8d4ceb89d3b0195a13b8230a460e5063de
Vitor Sales <vitormalisa@gmail.com>
2023-09-27 22:46:58 -0300

    Requisito 0 feito: Projeto Concluid

--- src/10-products-rich-in-vitamin.js
+++ src/10-products-rich-in-vitamin.js
@@ -1,7 +1,61 @@
 const stockProducts = require('./data.json');
 
+const vitaminsArray = (keys, values) => {
+  let kindOfVitamins = [];
+  for (let indexVitamin = 0; indexVitamin < keys.length; indexVitamin += 1) {
+    kindOfVitamins.push(`${keys[indexVitamin]} - ${values[indexVitamin]}`);
+  };
+  return kindOfVitamins;
+};
+
 const getProductsRichInVitamin = () => {
   // Desenvolva seu código dentro dessa função...
+  // Variaveis: arrays
+  const productsVitamins = [];
+  let kindOfVitamins = [];
+  let count = 0;
+  // Laço de repetição para achar esses produtos
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    let objectKeys = Object.keys(stockProducts[index].nutritionalInfo);
+    
+
+    if (objectKeys[objectKeys.length - 1] === 'vitamins') {
+      let vitaminKeys = Object.keys(stockProducts[index].nutritionalInfo.vitamins);
+      let vitaminValues = Object.values(stockProducts[index].nutritionalInfo.vitamins);
+      for (let indexVitamin = 0; indexVitamin < vitaminKeys.length; indexVitamin += 1) {
+        kindOfVitamins.push(`${vitaminKeys[indexVitamin]} - ${vitaminValues[indexVitamin]}`);
+      }
+      productsVitamins.push({
+        description: stockProducts[index].description,
+        formattedPrice: `R$ ${stockProducts[index].price}`,
+        vitaminsInformation: kindOfVitamins
+      });
+      kindOfVitamins = [];
+      
+
+      };
+    ;
+  };
+  // Condicional para adicionar produtos no array
+  // Usar construtor em uma nova variavel para dar uma array das vitaminas
+  // Laço de repetição para incluir as vitaminas
+  // console.log(kindOfVitamins);
+  // Retorno?
+  return productsVitamins;
 };
+
+
+
+// const ArrayOfVitamins = () => {
+//   const arrayOfArrays = [];
+//   const loopArray = [];
+//   for (let index = 0; index < stockProducts.length; index += 1) {
+//     let vitaminKeys = Object.keys(stockProducts[index].nutritionalInfo.vitamins);
+//     console.log(vitaminKeys);
+//   }
+
+// }
+// console.log(ArrayOfVitamins());
+console.log(getProductsRichInVitamin());
 
 module.exports = { getProductsRichInVitamin };
--- src/9-products-with-allergy-or-intolerance-info.js
+++ src/9-products-with-allergy-or-intolerance-info.js
@@ -1,8 +1,8 @@
 const stockProducts = require('./data.json');
 
 const getProductsWithAllergyOrIntoleranceInfo = () => {
-  // Desenvolva seu código dentro dessa função...
-  // variaveis: array
+// Desenvolva seu código dentro dessa função...
+// variaveis: array
   const allergicPointedOut = [];
   let allergicContent = 'Pode conter:';
   // Laço de repetição procurando propriedade de alergia ou intolerancia
@@ -10,14 +10,19 @@ const getProductsWithAllergyOrIntoleranceInfo = () => {
     allergicPointedOut.push({
       description: stockProducts[index].description,
       formattedPrice: `R$ ${stockProducts[index].price}`,
-    })
-    // transformando em array as chaves de cada objeto para procurar pela allegyOrIntolerance
+    });
+    // transformando em array as chaves de cada objeto 
+    // para procurar pela allegyOrIntolerance
     let objectKeysArray = Object.keys(stockProducts[index]);
     // Laço de repetição para checar se existe allergyOrIntolerance
     for (let indexKey = 0; indexKey < objectKeysArray.length; indexKey += 1) {
       if (objectKeysArray[indexKey] === 'allergyOrIntolerance') {
-        for (let indexAllergy = 0; indexAllergy < stockProducts[index].allergyOrIntolerance.length; indexAllergy += 1) {
-           allergicContent += ` ${stockProducts[index].allergyOrIntolerance[indexAllergy]}`;
+        for (
+          let indexAllergy = 0;
+          indexAllergy < stockProducts[index].allergyOrIntolerance.length;
+          indexAllergy += 1
+        ) {
+          allergicContent += ` ${stockProducts[index].allergyOrIntolerance[indexAllergy]}`;
         }
         allergicPointedOut[index].allergyOrIntoleranceMessage = allergicContent;
         allergicContent = 'Pode conter:';
